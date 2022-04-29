50b3a430d984e01e0b71a24a2f7055d01e8a9a1b
Raymond Campbell <104187574+RaymondC360@users.noreply.github.com>
2022-04-28 18:48:19 -0700

    Imported into PyCharm + More Cars

--- Cars.py
+++ Cars.py
@@ -7,12 +7,14 @@ class Car:
 
 
 car1 = Car(input("What color is your car? "), input("What model is your car? "))
-car2 = Car(input("What color is your second car? "), input("What mode is your second car? "))
+car2 = Car(input("What color is your second car? "), input("What model is your second car? "))
+car3 = Car(input("What color is your third car? "), input("What model is your third car? "))
 
 
 def listCars():
     print(car1.color, car1.model)
     print(car2.color, car2.model)
+    print(car3.color, car3.model)
 
 
 if __name__ == "__Cars__":
