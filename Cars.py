049a302cdc9aaae2469e80ab99d60fc67bff9019
Raymond Campbell <104187574+RaymondC360@users.noreply.github.com>
2022-04-29 11:28:31 -0700

    Update Cars.py

--- Cars.py
+++ Cars.py
@@ -1,20 +1,25 @@
 cars = []
+netWorth = 0
 
 
 class Car:
 
-    def __init__(self, color, model):
+    def __init__(self, color, model, value):
 
         self.color = color
         self.model = model
+        self.value = value
 
 
 amountOfCars = int(input("How many cars do you have?"))
 
-while amountOfCars > 0:
-    carInfo = Car(input("What color is your car? "), input("What model is your car? "))
-    cars.append(carInfo.color + carInfo.model)
+for i in range(amountOfCars):
+    carInfo = Car(input("What color is your car? "), input("What make is your car? "),
+                  float(input("What is the value of your car?")))
+    cars.append(carInfo.color + " " + carInfo.model)
+    netWorth = netWorth + carInfo.value
     print(amountOfCars)
     amountOfCars = amountOfCars - 1
-
+print("You own the following cars: \n")
 print(cars)
+print("With the following net worth: " + str("${:,.2f}".format(netWorth)))
